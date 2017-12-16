a92202aafe1da7c330363b501c080bf9f6ae7a84
Eric Sondergard <flamtap@users.noreply.github.com>
2017-12-16 15:06:46 -0700

    WIP: help dialog

---
+++ src/CouchExplorer/Features/Help/HelpDialogViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CouchExplorer.Features.Help
+{
+    public class HelpDialogViewModel : ViewModelBase
+    {
+        public string Version => Assembly.GetExecutingAssembly().GetName().Version.ToString();
+    }
+}
